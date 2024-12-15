26f4629e52bb9e2e450ba6b39186b9f644c234df
kennysunjaya <110735938+kennysunjaya@users.noreply.github.com>
2024-12-15 16:35:28 +0700

    Update mongodb.js

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,6 +1,7 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
+const uri = "mongodb+srv://kenn667914:YVA32Cf4Fjtz6rD7@cluster0.lfyxy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 
 export const client = new MongoClient(uri);
 export let db = null;
